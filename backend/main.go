6c98c670b137008d087e6bbf28bc173b69c57082
kazmathui <46832172+kaz-mathui@users.noreply.github.com>
2024-06-04 08:40:09 +0900

    apiちょっと試し

--- backend/main.go
+++ backend/main.go
@@ -13,5 +13,11 @@ func main() {
         })
     })
 
+    r.GET("/api/tanaka", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello, Tanaka!",
+        })
+    })
+
     r.Run() // デフォルトで :8080 で実行されます
 }
